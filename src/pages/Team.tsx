5fc83d0559832e296601fb47309e37ac2d18986e
kimkinam <kkl07092@gmail.com>
2025-10-28 23:50:53 +0900

    Mac mini

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,10 +1,9 @@
+import team_all_member from "../assets/team_all_member.webp";
 function Team() {
   return (
-    <section className="team page-section" id="team">
-      <div className="section-header">
-        <h2>Our Team</h2>
-        <p>Coming soon.</p>
-      </div>
+    <section className="team page-section" id="team" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* <img src="../assets/team_all_member.webp" alt="Team" /> */}
+      <img src={team_all_member} alt="Team" style={{ width: '50%', height: '50%', objectFit: 'contain' }} />
     </section>
   );
 }
